e4071eeda46a361a7f7bb19856f7306f68d63681
Risto Trajanov <risto.trajanov@gmail.com>
2020-06-12 16:10:10 +0200

    Text Summary React

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+/*global chrome*/
+import React, { Component } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        data: []
+    };
+  }
+
+  componentDidMount() {
+      var url = 'http://127.0.0.1:5000/main?url=';
+      chrome.tabs.query({
+              active: true,
+              currentWindow: true
+          },
+          (tabs) => {
+              url+=tabs[0].url;
+              console.log("Vo ComponentDidMount :  " + tabs[0].url);
+              this.fetchData(url);
+
+          });
+  }
+
+  fetchData(url){
+      fetch(url).then(result => {
+          console.log('Resault from fetch url: ');
+          console.log(result);
+          return result.json();
+      }).then(data => {
+          console.log('Data vo then od fetch: ');
+          console.log(data);
+          this.setState({data: data.summary});
+          console.log('State: ')
+          console.log(this.state.data)
+      })
+  }
+
+  render() {
+
+    return (
+        <div class='jumbotron'>
+            <h1 class='display-4'>Text Summary for the current page</h1>
+            <hr class='my-4'></hr>
+            <p> {this.state.data} </p>
+        </div>
+    );
+  }
 }
 
 export default App;
